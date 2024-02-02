f4c54922ceb25c1e53677052f364a46d73c3daf0
tonymtz <atony.mtz@gmail.com>
2024-02-02 16:54:43 -0700

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Label } from '~/components/label.component'
+
+export default function Home () {
+  return (
+    <main>
+      <h1>Loyalty</h1>
+      <Label />
+    </main>
+  )
+}
---
+++ src/components/label.component.tsx
@@ -0,0 +1,5 @@
+import { css } from '../../styled-system/css'
+
+export const Label = () => {
+  return <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
+}
