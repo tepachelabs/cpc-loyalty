b40000f142f987a5fa8f2af785d86b5afb3b8525
tonymtz <atony.mtz@gmail.com>
2024-02-03 18:53:43 -0700

    adds logger to order endpoint

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,3 +1,4 @@
+import { log } from '@logtail/next'
 import type { Order } from '@prisma/client'
 import { NextRequest } from 'next/server'
 
@@ -6,19 +7,25 @@ import prisma from '~/lib/prisma'
 export const dynamic = 'force-dynamic' // defaults to force-static
 
 export async function POST (req: NextRequest) {
-  const payload = await req.json()
+  try {
+    const payload = await req.json()
 
-  const order: Omit<Order, 'id' | 'updatedAt'> = {
-    shopifyOrderId: `${ payload.id }`,
-    createdAt: payload.created_at,
-    email: payload.email,
-    orderNumber: payload.order_number,
-    phone: payload.phone,
-    test: payload.test,
-    total: payload.total_price,
+    const order: Omit<Order, 'id' | 'updatedAt'> = {
+      shopifyOrderId: `${ payload.id }`,
+      createdAt: payload.created_at,
+      email: payload.email,
+      orderNumber: payload.order_number,
+      phone: payload.phone,
+      test: payload.test,
+      total: payload.total_price,
+    }
+
+    log.info('Storing new order', order)
+    await prisma.order.create({ data: order })
+
+    return new Response('ok')
+  } catch (e) {
+    log.error(e as unknown as any)
+    return new Response('error', { status: 500 })
   }
-
-  await prisma.order.create({ data: order })
-
-  return new Response('ok')
 }
