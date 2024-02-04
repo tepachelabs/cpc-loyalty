821fb8505c0102ed5a5c78f58f772784a6db3846
tonymtz <atony.mtz@gmail.com>
2024-02-04 11:36:29 -0700

    show wallets and orders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,64 @@
-import { log } from '@logtail/next'
+import { Order, Wallet } from '@prisma/client'
 
-import { Button } from '~/components/button.component'
+import { fetchFromPath } from '~/lib/utils'
 
-export default function Home () {
+async function getData () {
+  const [{ orders }, { wallets }] = await Promise.all([
+    fetchFromPath('http://localhost:3000/api/orders'),
+    fetchFromPath('http://localhost:3000/api/wallets'),
+  ])
+
+  return { orders, wallets }
+}
+
+export default async function Home () {
+  const { orders, wallets } = await getData()
+
   return (
     <main>
       <h1>Loyalty</h1>
-      <Button/>
+
+      <hr/>
+
+      <h2>Orders</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          { orders.map((order: Order) => (
+            <tr key={ order.id }>
+              <td>{ order.id }</td>
+              <td>{ order.total }</td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
+
+      <hr/>
+
+      <h2>Wallets</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          { wallets.map((wallet: Wallet) => (
+            <tr key={ wallet.id }>
+              <td>{ wallet.id }</td>
+              <td>{ wallet.credits }</td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
     </main>
   )
 }
